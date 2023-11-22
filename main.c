be8e724517a923660876d8b5dd93b52d6946a060
mateussalvador <mateuslimasalvador@gmail.com>
2023-11-22 16:29:01 -0300

    criando os arquivos e inicializando o main

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("\tPor Breno Reboredo, Gabriel Damasio, Leoanrdo Duarte, Mateus Salvador e Yann Soares\n\n");
+
+    int resultado;
+
+    printf("------------ MENU INICIAL ------------\n");
+        printf("1 - Inicializar\n");
+        printf("2 - Tutorial\n");
+        printf("3 - Sair\n");
+
+        scanf("%d", &resultado);
+
+    return 0;
+}
