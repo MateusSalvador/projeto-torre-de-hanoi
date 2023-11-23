62c213c42f0d2993834bd448fa9bc2360f4159e6
BrenoReboredo-09 <127802083+BrenoReboredo-09@users.noreply.github.com>
2023-11-23 20:50:22 -0300

    adicionado os comentários na pilha.h

--- Pilha.h
+++ Pilha.h
@@ -1,17 +1,26 @@
+// Definição de um tipo abstrato de dados (TAD) chamado Pilha
 typedef struct pilha Pilha;
 
+// Protótipo da função para criar uma nova pilha
 Pilha * criar();
 
+// Protótipo da função para destruir uma pilha, liberando a memória alocada
 void destruir(Pilha * p);
 
+// Protótipo da função para desempilhar (remover e obter) o elemento do topo da pilha
 int desempilhar(Pilha * p);
 
+// Protótipo da função para empilhar (adicionar) um elemento no topo da pilha
 void empilhar(Pilha * p, int x);
 
+// Protótipo da função para obter o tamanho atual da pilha (número de elementos)
 int tamanho(Pilha * p);
 
+// Protótipo da função para obter o elemento do topo da pilha sem removê-lo
 int topo(Pilha * p);
 
+// Protótipo da função para imprimir os elementos da pilha
 void imprimir(Pilha * p);
 
+// Protótipo da função para inverter a ordem dos três primeiros elementos da pilha
 void inverter3(Pilha * p);
