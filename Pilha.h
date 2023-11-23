6d60c33349ba392e6e3af1926d4c66f6d89b7a5a
BrenoReboredo-09 <127802083+BrenoReboredo-09@users.noreply.github.com>
2023-11-22 22:09:19 -0300

    Update Pilha.h

---
+++ Pilha.h
@@ -0,0 +1,17 @@
+typedef struct pilha Pilha;
+
+Pilha * criar();
+
+void destruir(Pilha * p);
+
+int desempilhar(Pilha * p);
+
+void empilhar(Pilha * p, int x);
+
+int tamanho(Pilha * p);
+
+int topo(Pilha * p);
+
+void imprimir(Pilha * p);
+
+void inverter3(Pilha * p);
